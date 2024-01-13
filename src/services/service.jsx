3240518aa4750fcb3fe59d3ba3465afbf4ccb511
carlos <cdiez1995@gmail.com>
2024-01-14 00:10:39 +0100

    service for get

---
+++ src/services/service.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useHttpGet = (url, params) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url, { params });
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url, params]);
+
+  return { loading, data, error };
+};
+
+export default useHttpGet;
