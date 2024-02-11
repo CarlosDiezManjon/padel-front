799b5daaa16769718d48a8dd1d7f30d7dfdc32ea
carlos <cdiez1995@gmail.com>
2024-02-11 21:42:03 +0100

    prueba

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,11 +6,16 @@ export default function Footer() {
   const currentTab = useStore((state) => state.currentTab)
   const setCurrentTab = useStore((state) => state.setCurrentTab)
   const user = useStore((state) => state.user)
+  const state = useStore((state) => state)
 
   const handleChangeTab = useCallback((index) => {
-    console.log('index', index)
     setCurrentTab(index)
   }, [])
+
+  useEffect(() => {
+    console.log('loading', state.isLoading)
+    console.log('currentTab', state.currentTab)
+  }, [state])
 
   const listFooter = [
     {
@@ -52,7 +57,7 @@ export default function Footer() {
             <Link
               key={index}
               className={
-                'h-full text-3xl w-20 flex flex-col justify-center mx-1 rounded font-medium py-2 cursor-pointer z-50' +
+                'h-full text-3xl w-20 flex flex-col justify-center mx-1 rounded font-medium py-2 transition duration-700 cursor-pointer z-50' +
                 (index === currentTab ? ' text-xl' : '')
               }
               onClick={() => handleChangeTab(index)}
@@ -60,7 +65,7 @@ export default function Footer() {
             >
               <div
                 className={
-                  'flex flex-col items-center ' +
+                  'flex flex-col items-center transition-all duration-700 ' +
                   (index === currentTab ? 'text-2xl justify-center ' : 'text-3xl pt-4')
                 }
               >
@@ -70,7 +75,7 @@ export default function Footer() {
                     index === currentTab
                       ? 'transform translate-y-0.5 opacity-1 text-base'
                       : 'transform translate-y-4 opacity-0 ' +
-                        ' text-base flex transform translate-y-6'
+                        'transition-all duration-700 text-base flex transform translate-y-6'
                   }
                 >
                   {item.label}
